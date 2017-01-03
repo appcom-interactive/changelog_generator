9064be47507c1764d7401f42c6dd7920939ca7e9
Stefan Neidig <s.neidig@appcom-interactive.de>
2017-01-03 17:55:52 +0100

    Check if working dir is valid git repo before creating changelog

--- lib/git_changelog_generator.rb
+++ lib/git_changelog_generator.rb
@@ -1,31 +1,38 @@
 require "git_changelog_generator/version"
-require 'git'
 
 module GitChangelogGenerator
 
   def self.generate_changelog output
-    tags = (`git tag --sort=taggerdate --list --format='%(refname:short)'`.split("\n") << "HEAD").reverse
-    message = ''
+    is_git_directory = File.directory? '.git'
 
-    tags.each_with_index { |tag, index|
-      if index < tags.length-1
-        cmd =`git log #{tags[index+1]}...#{tag} --format="  * %s (%an <%ae>)"`
-      else
-        cmd = `git log #{tag} --format="  * %s (%an <%ae>)"`
-      end
+    if is_git_directory
+      tags = (`git tag --sort=taggerdate --list --format='%(refname:short)'`.split("\n") << "HEAD").reverse
+      message = ''
 
-      unless cmd.strip.empty?
-        message += index == 0 ? "Version #{tag} (" : "\nVersion #{tag[0..-1]} ("
-        message += `git log #{tag} --format='%aD' -1`
-        message = message[0..-2] + "):\n\n"
+      tags.each_with_index { |tag, index|
+        if index < tags.length-1
+          cmd =`git log #{tags[index+1]}...#{tag} --format="  * %s (%an <%ae>)"`
+        else
+          cmd = `git log #{tag} --format="  * %s (%an <%ae>)"`
+        end
 
-        message += cmd
-      end
-    }
+        unless cmd.strip.empty?
+          message += index == 0 ? "Version #{tag} (" : "\nVersion #{tag[0..-1]} ("
+          message += `git log #{tag} --format='%aD' -1`
+          message = message[0..-2] + "):\n\n"
 
-    File.open(output, 'wb+') { |f|
-      f << message
-    }
+          message += cmd
+        end
+      }
+
+      File.open(output, 'wb+') { |f|
+        f << message
+      }
+
+      puts "Created changelog. File is available under #{File.expand_path output}"
+    else
+      puts 'Not a git repository. Run this command inside a valid git directory.'
+    end
   end
 
 end
